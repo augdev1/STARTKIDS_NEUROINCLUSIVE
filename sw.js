b0eacdb97f38d9e873cbfdb5e61a3ad11ef690eb
augdev1 <augustosousa1798@gmail.com>
2026-09-11 07:25:05 -0400

    chore(cache): atualiza cache do Service Worker e versao de assets para v2

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
    tela cheia e carregue instantaneamente mesmo em redes lentas.
    ========================================================================== */
 
-const CACHE_NAME = 'startkids-cache-v1';
+const CACHE_NAME = 'startkids-cache-v2';
 const PRECACHE_ASSETS = [
   '/',
   '/login.html',
