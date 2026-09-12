9bac93990de26bd4dfbafc946e00303efc583689
augdev1 <augustosousa1798@gmail.com>
2026-09-12 09:06:46 -0400

    design(pwa): remove texto da logo deixando apenas arvore e arco-iris como logotipo

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
    tela cheia e carregue instantaneamente mesmo em redes lentas.
    ========================================================================== */
 
-const CACHE_NAME = 'startkids-cache-v14';
+const CACHE_NAME = 'startkids-cache-v15';
 const PRECACHE_ASSETS = [
   '/',
   '/login.html',
